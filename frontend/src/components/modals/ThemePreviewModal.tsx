103bbecd9c42f9860be562c4616eaec485a690d1
Arshil Hapani <arshilhapani@Arshils-MacBook-Pro.local>
2025-04-14 19:11:25 +0530

    fixed default theme issue

--- frontend/src/components/modals/ThemePreviewModal.tsx
+++ frontend/src/components/modals/ThemePreviewModal.tsx
@@ -13,7 +13,16 @@ const ThemePreviewModal = ({ theme }: Props) => {
   return (
     <Modal type={`theme-preview-${theme.theme_id}`}>
       <div>
-        <ComponentPreview />
+        <ComponentPreview
+          initialTheme={{
+            background_color: theme.background_color,
+            primary_color: theme.primary_color,
+            secondary_color: theme.secondary_color,
+            surface_color: theme.surface_color,
+            text_color: theme.text_color,
+            theme_id: Number(theme.theme_id),
+          }}
+        />
       </div>
     </Modal>
   );
